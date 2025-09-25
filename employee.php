71bded5f5d1d3ccc8c802b183429ab3c409734a9
JARP <105678913+anderson895@users.noreply.github.com>
2025-09-25 23:27:40 +0800

    first commit

---
+++ employee.php
@@ -0,0 +1,35 @@
+<!doctype html>
+<html>
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link href="src/output.css" rel="stylesheet">
+    <title>RevvedUp</title>
+</head>
+<body class="min-h-screen bg-gradient-to-b from-black to-red-900 flex flex-col items-center text-white relative">
+<!-- Navbar -->
+<div class="absolute top-4 right-6 flex items-center gap-4">
+    <a href="#" class="text-gray-300 hover:text-white text-sm">Log In as</a>
+    <a href="admin" class="text-gray-300 hover:text-white text-sm">Admin</a>
+    <a href="employee" class="text-white font-semibold border-b-2 border-white text-sm">Employee</a>
+</div>
+
+
+
+<!-- Container for Logo and Form -->
+<div class="flex flex-col items-center justify-center flex-grow -mt-16"> 
+  <!-- Logo -->
+  <img src="static/images/login_logo.png" alt="RevvedUp Logo" class="mx-auto w-56 mb-2">
+
+  <!-- Login Form -->
+  <div class="bg-transparent p-6 rounded-lg w-80">
+      <h1 class="text-2xl font-bold text-center mb-4">Employee</h1>
+      <form action="#" method="POST" class="flex flex-col space-y-4">
+        <input type="text" placeholder="PIN" class="px-4 py-2 rounded bg-white text-black placeholder-gray-500 focus:outline-none">
+        <button type="submit" class="bg-white text-black font-semibold py-2 rounded hover:bg-gray-200">Login</button>
+        <a href="#" class="underline text-xs text-gray-300 hover:text-white text-center">Forgot PIN</a>
+      </form>
+  </div>
+</div>
+</body>
+</html>
