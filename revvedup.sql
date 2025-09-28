38bb27c25f8c3899951be4b09abfd1bb65c44cba
JARP <105678913+anderson895@users.noreply.github.com>
2025-09-28 23:25:17 +0800

    update

--- revvedup.sql
+++ revvedup.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Sep 27, 2025 at 07:37 AM
+-- Generation Time: Sep 28, 2025 at 05:24 PM
 -- Server version: 10.4.32-MariaDB
 -- PHP Version: 8.2.12
 
@@ -78,11 +78,11 @@ CREATE TABLE `product` (
 --
 
 INSERT INTO `product` (`prod_id`, `prod_name`, `prod_capital`, `prod_price`, `prod_qty`, `prod_img`, `prod_status`) VALUES
-(3, 'product 2', 5.00, 11.00, 210, 'item_68d60b3664f741.76866050.png', 1),
-(4, 'product 1', 130.00, 150.00, 95, 'item_68d6430495df58.36133211.jpg', 1),
-(5, 'product 3', 280.00, 300.00, 70, 'item_68d643420bba08.70461903.jpg', 1),
-(6, 'product 6', 50.00, 55.00, 60, 'item_68d665eca329d5.22334298.jpg', 1),
-(7, 'test', 500.00, 600.00, 100, 'item_68d776d7db06b6.21276657.jpg', 1);
+(3, 'product 2', 5.00, 11.00, 205, 'item_68d60b3664f741.76866050.png', 1),
+(4, 'product 1', 130.00, 150.00, 87, 'item_68d6430495df58.36133211.jpg', 1),
+(5, 'product 3', 280.00, 300.00, 64, 'item_68d643420bba08.70461903.jpg', 1),
+(6, 'product 6', 50.00, 55.00, 54, 'item_68d77ad7dc2db3.03460954.avif', 1),
+(7, 'test', 500.00, 600.00, 99, 'item_68d776d7db06b6.21276657.jpg', 1);
 
 -- --------------------------------------------------------
 
@@ -122,22 +122,14 @@ CREATE TABLE `transaction` (
 --
 
 INSERT INTO `transaction` (`transaction_id`, `transaction_date`, `transaction_service`, `transaction_item`, `transaction_discount`, `transaction_vat`, `transaction_total`, `transaction_payment`, `transaction_change`, `transaction_status`) VALUES
-(19, '2025-09-26 15:14:56', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\",\"prod_id\":\"3\"},{\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\",\"prod_id\":\"5\"}]', 0.00, 109.32, 1270.32, 10000.00, 8729.68, 1),
-(20, '2025-09-26 15:16:50', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\",\"prod_id\":\"3\"},{\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\",\"prod_id\":\"5\"}]', 0.00, 109.32, 1270.32, 10000.00, 8729.68, 1),
-(21, '2025-09-26 15:18:55', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\",\"prod_id\":\"3\"},{\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\",\"prod_id\":\"5\"}]', 100.00, 97.32, 1158.32, 10000.00, 8841.68, 1),
-(22, '2025-09-26 15:20:16', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\"}]', 0.00, 109.32, 1270.32, 10000.00, 8729.68, 1),
-(23, '2025-09-26 15:20:25', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\"}]', 0.00, 109.32, 1270.32, 0.00, 0.00, 1),
-(24, '2025-09-26 15:20:58', '[{\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\"}]', 0.00, 109.32, 1270.32, 10000.00, 8729.68, 1),
-(25, '2025-09-26 15:24:01', '[{\"service_id\":\"12\",\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"3\"},{\"service_id\":\"11\",\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"3\"},{\"service_id\":\"10\",\"name\":\"change all\",\"price\":\"100\",\"emp_id\":\"4\"}]', '[{\"item_id\":\"13\",\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"item_id\":\"12\",\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"3\",\"subtotal\":\"900\"}]', 0.00, 109.32, 1270.32, 10000.00, 8729.68, 1),
-(26, '2025-09-26 16:07:42', '[{\"service_id\":\"17\",\"name\":\"parking\",\"price\":\"50\",\"emp_id\":\"1\"},{\"service_id\":\"16\",\"name\":\"painting\",\"price\":\"100\",\"emp_id\":\"1\"},{\"service_id\":\"18\",\"name\":\"washing\",\"price\":\"50\",\"emp_id\":\"3\"}]', '[{\"item_id\":\"15\",\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"item_id\":\"14\",\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"1\",\"subtotal\":\"300\"}]', 0.00, 37.32, 548.32, 800.00, 251.68, 1),
-(27, '2025-09-27 00:09:35', '[{\"service_id\":\"21\",\"name\":\"gear oil\",\"price\":\"99\",\"emp_id\":\"1\"},{\"service_id\":\"20\",\"name\":\"car wash\",\"price\":\"50\",\"emp_id\":\"2\"},{\"service_id\":\"19\",\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"2\"}]', '[{\"item_id\":\"17\",\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"8\",\"subtotal\":\"2400\"},{\"item_id\":\"16\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\"}]', 0.00, 306.00, 3105.00, 10000.00, 6895.00, 1),
-(28, '2025-09-27 00:12:12', '[{\"service_id\":\"22\",\"name\":\"service 1\",\"price\":\"999\",\"emp_id\":\"2\"}]', '[{\"item_id\":\"21\",\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"1\",\"subtotal\":\"11\"},{\"item_id\":\"19\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\"}]', 0.00, 19.32, 1179.32, 1200.00, 20.68, 1),
-(29, '2025-09-27 00:15:18', '[]', '[{\"item_id\":\"22\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\"}]', 0.00, 18.00, 168.00, 1000.00, 832.00, 1),
-(30, '2025-09-27 00:16:35', '[]', '[{\"item_id\":\"23\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\"}]', 0.00, 18.00, 168.00, 200.00, 32.00, 1),
-(31, '2025-09-27 00:16:54', '[]', '[{\"item_id\":\"24\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\"}]', 0.00, 18.00, 168.00, 200.00, 32.00, 1),
-(32, '2025-09-27 00:17:43', '[]', '[{\"item_id\":\"25\",\"prod_id\":\"6\",\"name\":\"product 6\",\"qty\":\"66\",\"subtotal\":\"3630\"}]', 0.00, 435.60, 4065.60, 5000.00, 934.40, 1),
-(33, '2025-09-27 00:18:50', '[]', '[{\"item_id\":\"26\",\"prod_id\":\"6\",\"name\":\"product 6\",\"qty\":\"6\",\"subtotal\":\"330\"}]', 0.00, 39.60, 369.60, 400.00, 30.40, 1),
-(34, '2025-09-27 00:25:19', '[{\"service_id\":\"23\",\"name\":\"change oil\",\"price\":\"50\",\"emp_id\":\"4\"}]', '[]', 0.00, 0.00, 50.00, 100.00, 50.00, 1);
+(36, '2025-09-27 06:02:45', '[]', '[{\"item_id\":\"29\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\",\"capital\":\"130\"},{\"item_id\":\"28\",\"prod_id\":\"6\",\"name\":\"product 6\",\"qty\":\"5\",\"subtotal\":\"275\",\"capital\":\"50\"}]', 0.00, 51.00, 476.00, 500.00, 24.00, 1),
+(37, '2025-09-27 06:34:25', '[]', '[{\"item_id\":\"30\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\",\"capital\":\"130\"}]', 0.00, 18.00, 168.00, 200.00, 32.00, 1),
+(38, '2025-09-27 06:36:49', '[{\"service_id\":\"24\",\"name\":\"chang oil\",\"price\":\"100\",\"emp_id\":\"2\"}]', '[{\"item_id\":\"33\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"1\",\"subtotal\":\"150\",\"capital\":\"130\"},{\"item_id\":\"32\",\"prod_id\":\"4\",\"name\":\"product 1\",\"qty\":\"5\",\"subtotal\":\"750\",\"capital\":\"130\"},{\"item_id\":\"31\",\"prod_id\":\"6\",\"name\":\"product 6\",\"qty\":\"1\",\"subtotal\":\"55\",\"capital\":\"50\"}]', 0.00, 114.60, 1169.60, 1200.00, 30.40, 1),
+(39, '2025-10-27 06:38:06', '[]', '[{\"item_id\":\"35\",\"prod_id\":\"3\",\"name\":\"product 2\",\"qty\":\"5\",\"subtotal\":\"55\",\"capital\":\"5\"},{\"item_id\":\"34\",\"prod_id\":\"7\",\"name\":\"test\",\"qty\":\"1\",\"subtotal\":\"600\",\"capital\":\"500\"}]', 0.00, 78.60, 733.60, 750.00, 16.40, 1),
+(40, '2025-09-28 14:14:08', '[{\"service_id\":\"25\",\"name\":\"change color\",\"price\":\"500\",\"emp_id\":\"3\"}]', '[{\"item_id\":\"36\",\"prod_id\":\"5\",\"name\":\"product 3\",\"qty\":\"5\",\"subtotal\":\"1500\",\"capital\":\"280\"}]', 0.00, 180.00, 2180.00, 2200.00, 20.00, 1),
+(41, '2025-09-28 14:28:21', '[{\"service_id\":\"28\",\"name\":\"cvt cleaning\",\"price\":\"300\",\"emp_id\":\"1\"},{\"service_id\":\"26\",\"name\":\"change oil\",\"price\":\"100\",\"emp_id\":\"1\"},{\"service_id\":\"27\",\"name\":\"gear oil\",\"price\":\"50\",\"emp_id\":\"4\"}]', '[]', 0.00, 0.00, 450.00, 500.00, 50.00, 1),
+(42, '2025-09-28 14:29:33', '[{\"service_id\":\"29\",\"name\":\"bulcanize\",\"price\":\"60\",\"emp_id\":\"1\"}]', '[]', 0.00, 0.00, 60.00, 100.00, 40.00, 1),
+(43, '2025-09-25 15:22:13', '[{\"service_id\":\"30\",\"name\":\"cvt repair\",\"price\":\"1500\",\"emp_id\":\"1\"}]', '[]', 0.00, 0.00, 1500.00, 15000.00, 13500.00, 1);
 
 -- --------------------------------------------------------
 
@@ -163,6 +155,29 @@ CREATE TABLE `user` (
 INSERT INTO `user` (`user_id`, `firstname`, `lastname`, `username`, `password`, `pin`, `position`, `status`) VALUES
 (1, 'juan', 'dela cruz', 'admin', '$2a$12$jsAMWl2OcxUyo8OP7mLH4uTRYd5ln04QTtjU5O3qS1Q5N1o6f6yzm', NULL, 'admin', 1),
 (7, 'joshua', 'padilla', NULL, NULL, 12345, 'employee', 1);
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `weeks`
+--
+
+CREATE TABLE `weeks` (
+  `week_id` int(11) NOT NULL,
+  `week_number` int(11) NOT NULL,
+  `week_transaction_id` int(11) NOT NULL,
+  `week_deduction` decimal(10,2) NOT NULL DEFAULT 0.00
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+
+--
+-- Dumping data for table `weeks`
+--
+
+INSERT INTO `weeks` (`week_id`, `week_number`, `week_transaction_id`, `week_deduction`) VALUES
+(1, 39, 40, 0.00),
+(2, 39, 41, 50.00),
+(3, 39, 42, 0.00),
+(4, 39, 43, 0.00);
 
 --
 -- Indexes for dumped tables
@@ -207,6 +222,13 @@ ALTER TABLE `user`
   ADD PRIMARY KEY (`user_id`);
 
 --
+-- Indexes for table `weeks`
+--
+ALTER TABLE `weeks`
+  ADD PRIMARY KEY (`week_id`),
+  ADD KEY `week_transaction_id` (`week_transaction_id`);
+
+--
 -- AUTO_INCREMENT for dumped tables
 --
 
@@ -220,7 +242,7 @@ ALTER TABLE `employee`
 -- AUTO_INCREMENT for table `item_cart`
 --
 ALTER TABLE `item_cart`
-  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=27;
+  MODIFY `item_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=37;
 
 --
 -- AUTO_INCREMENT for table `product`
@@ -232,19 +254,25 @@ ALTER TABLE `product`
 -- AUTO_INCREMENT for table `service_cart`
 --
 ALTER TABLE `service_cart`
-  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=24;
+  MODIFY `service_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=31;
 
 --
 -- AUTO_INCREMENT for table `transaction`
 --
 ALTER TABLE `transaction`
-  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=35;
+  MODIFY `transaction_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=44;
 
 --
 -- AUTO_INCREMENT for table `user`
 --
 ALTER TABLE `user`
   MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+
+--
+-- AUTO_INCREMENT for table `weeks`
+--
+ALTER TABLE `weeks`
+  MODIFY `week_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
 
 --
 -- Constraints for dumped tables
@@ -256,6 +284,12 @@ ALTER TABLE `user`
 ALTER TABLE `service_cart`
   ADD CONSTRAINT `service_cart_ibfk_1` FOREIGN KEY (`service_employee_id`) REFERENCES `employee` (`emp_id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `service_cart_ibfk_2` FOREIGN KEY (`service_user_id`) REFERENCES `user` (`user_id`) ON DELETE CASCADE ON UPDATE CASCADE;
+
+--
+-- Constraints for table `weeks`
+--
+ALTER TABLE `weeks`
+  ADD CONSTRAINT `weeks_ibfk_1` FOREIGN KEY (`week_transaction_id`) REFERENCES `transaction` (`transaction_id`) ON DELETE CASCADE ON UPDATE CASCADE;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
